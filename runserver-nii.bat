630cd0643b0101b16be04c53502dd8de15f1af64
ivansglukhov <ivan.s.glukhov@gmail.com.com>
2026-08-03 00:20:25 +0300

    Restrict NII employees to humans

--- runserver-nii.bat
+++ runserver-nii.bat
@@ -1,3 +1,3 @@
 @echo off
-dotnet run --project Content.Server -- --cvar config.presets=Nii/nii --cvar game.lobbyduration=10
+dotnet run --project Content.Server -- --cvar config.presets=Nii/nii --cvar game.lobbyduration=10 --cvar game.role_timers=true
 pause
